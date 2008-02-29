89378342acdeb6fc9d8b9e3494998339a474bbdc
Alex Egg <eggie5@eggie5.local>
2008-02-29 12:51:27 -0800

    updated readme and code

--- lib/acts_as_exclusive.rb
+++ lib/acts_as_exclusive.rb
@@ -3,27 +3,37 @@ require 'active_record'
 module BearBrand
   module Acts
     module Exclusive
+      # included is called from the ActiveRecord::Base when you inject this module
       def self.included(base)
+        # Add acts_as_exclusive availability by extending the module that owns the function.
         base.extend(ClassMethods)  
       end
       
       module ClassMethods
-        def acts_as_exclusive
+        def acts_as_exclusive(options)
           extend Exclusive::SingletonMethods
-          include Exclusive::InstanceMethods
+          
+          @conditions_string=options[:conditions]
+          @joins=options[:joins]
         end
       end
       
-      # add your class methods here
       module SingletonMethods
-        def cluck
-          'cluck'
-        end
-      end
-      
-      module InstanceMethods
-        def say
-          "I'm speaking"
+        #exclusive search - wrapper around AR::B#find
+        #Expects an owner key in options hash
+        def search(options)
+          owner = options[:owner]
+          conditions = [@conditions_string, owner.id] #array
+          extra_conditions = options[:conditions] #array
+          
+          #hackish weird way to add another AR condition
+          if extra_conditions and ! extra_conditions.empty?
+            conditions[0]+=" AND #{extra_conditions[0]}"
+            #fails if more then 1 ec is provided
+            conditions.push(extra_conditions[1])
+          end
+          
+          find(:all, :joins => @joins, :conditions => conditions)
         end
       end
     end
