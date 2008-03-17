c8608ad7d4ae153c5ff6aa2ba05f6c3d7249e808
eggie5 <eggie5@eggie5.local>
2008-03-17 10:24:54 -0700

    changes instance method signatures

--- lib/acts_as_exclusive.rb
+++ lib/acts_as_exclusive.rb
@@ -17,14 +17,32 @@ module BearBrand
       end
       
       module ClassMethods
+        #Property.find(:all, :exclusive_to => @user.id)
         def find(*args)
-          if(args.first.to_s == "all_exclusive_to")
-            raise ArgumentError.new("You must provide a :owner argument") if args[1].blank?
-            owner_id = args[1][:owner]
-            super(:all, :joins => options[:joins], :conditions => [options[:conditions], owner_id])
+          if(args[0].to_s == "all" and args[1].to_s.include?("exclusive_to"))
+            local_options = args.extract_options!
+            
+            conditions = [options[:conditions], local_options[:exclusive_to]]
+            
+            #merge local_options conditions hash with same in options
+            dynamic_conditions = local_options[:conditions] #array
+            if dynamic_conditions and ! dynamic_conditions.empty?
+              conditions[0]+=" AND #{dynamic_conditions[0]}"
+              #fails if more then 1 ec is provided
+              conditions.push(dynamic_conditions[1])
+            end
+            
+            super(:all, :joins => options[:joins], :conditions => conditions)
           else
             super
           end
+        end
+        
+        #alias for find(:all, :exclusive_to => @user.id)
+        #Property.find_all_exclusive_to(:owner => @user.id)
+        def find_all_exclusive_to(args={})
+          #translat this to: find(:all, :exclusive_to => id)
+          find(:all, :exclusive_to => args[:owner_id], :conditions => args[:conditions])
         end
       end
     end
